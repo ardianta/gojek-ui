fddbfa9d2e160917e5f8dbd0a09d87ac696e4cc1
ahmad.muhardian <ahmad.muhardian@gmail.com>
2024-09-08 23:02:41 +0000

    make _newsCard as private method fo NewsFeed widget class

--- lib/components/news_feed.dart
+++ lib/components/news_feed.dart
@@ -6,6 +6,42 @@ import 'package:gojek/models/news.dart';
 class NewsFeed extends StatelessWidget {
   const NewsFeed({super.key});
 
+  Widget _newsCard({required News news}) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 24, left: 16, right: 16),
+      child: Container(
+        clipBehavior: Clip.hardEdge,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            border: Border.all(color: dark4)),
+        child: Column(
+          children: [
+            Image.asset('assets/images/${news.image}'),
+            Padding(
+              padding: const EdgeInsets.all(24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    news.title,
+                    style: bold16.copyWith(color: dark1),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    news.description,
+                    style: regular14.copyWith(color: dark2),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -39,54 +75,11 @@ class NewsFeed extends StatelessWidget {
               ),
 
               // News Items
-              ...news.map((item) => NewsCard(news: item))
+              ...news.map((item) => _newsCard(news: item))
             ],
           ),
         )
       ],
-    );
-  }
-}
-
-class NewsCard extends StatelessWidget {
-  final News news;
-
-  const NewsCard({super.key, required this.news});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 24, left: 16, right: 16),
-      child: Container(
-        clipBehavior: Clip.hardEdge,
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15),
-            border: Border.all(color: dark4)),
-        child: Column(
-          children: [
-            Image.asset('assets/images/${news.image}'),
-            Padding(
-              padding: const EdgeInsets.all(24),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    news.title,
-                    style: bold16.copyWith(color: dark1),
-                  ),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  Text(
-                    news.description,
-                    style: regular14.copyWith(color: dark2),
-                  )
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
